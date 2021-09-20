fa7007514b01bdec5fbd1d288a2163d806850949
Brett Kaliner <brett.kaliner149@gmail.com>
2021-09-20 16:13:25 -0400

    First commit

--- Flash-test.py
+++ Flash-test.py
@@ -4,7 +4,14 @@ app = Flask(__name__) #"__main__"
 
 @app.route('/hello', methods = ['GET'])
 def hello_world():
-    return "Hello World"
+    return """<html>
+    <body>
+    <h1>Hello World</h1>
+    
+    <p> this is a paragraph of text.<p>
+    </body>
+    </html>
+    """
 
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', port = 8080)
